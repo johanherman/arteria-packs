4cecbb076ca3e90f38eb5216fe782e1bf0770dd0
Johan Hermansson <johan.hermansson@medsci.uu.se>
2017-01-12 16:34:23 +0100

    Refactor polling to always return the JSON object in the St2 result field instead

--- actions/poll_status.py
+++ actions/poll_status.py
@@ -52,9 +52,9 @@ class PollStatus(Action):
             if irma_mode:
                 modified_link = _rewrite_link(response_json['link'])
                 self.logger.info("In irma mode, will rewrite link to: {}".format(modified_link))
-                return modified_link
+                return {"response": response_json, "url": modified_link}
             else:
-                return response_json['link']
+                return {"response": response_json, "url": response_json['link']}
         except RequestException as err:
             self.logger.error("An error was encountered when trying to "
                                 "post to url: {0}, {1}".format(endpoint, err))
@@ -126,7 +126,9 @@ class PollStatus(Action):
                 return False, json_resp
 
     def run(self, url, body, sleep, ignore_result, irma_mode, verify_ssl_cert, max_retries=3):
-        status_link = self.post_to_endpoint(url, body, irma_mode, verify_ssl_cert)
-        return self.check_status(status_link, sleep, ignore_result, verify_ssl_cert, max_retries)
+        start_response = self.post_to_endpoint(url, body, irma_mode, verify_ssl_cert) 
+        status_links = start_response['url']
+        status_val, status_response = self.check_status(status_link, sleep, ignore_result, verify_ssl_cert, max_retries)
+        return status_val, { "response_from_start": start_response, "response_from_last_status_check": status_response }
 
 
